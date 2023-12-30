ba55c03205ffac9ad5033665966b2a79b5d87df3
prajwalsoni <prajwalsonics@gmail.com>
2023-12-30 14:56:39 +0530

    Added Filter data and Updated Product and Product Card

--- src/customer/components/Product/ProductCard.jsx
+++ src/customer/components/Product/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({ product }) => {
             </div>
             <div className='textPart bg-white p-3'>
                 <div>
-                    <p className='font-bold opacity-60'>{product.price}</p>
+                    <p className='font-bold opacity-60'> ₹{product.price}</p>
                     <p>{product.title}</p>
                 </div>
                 <div className='flex items-center space-x-2' >
