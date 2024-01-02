09e4b5b17bd6d3ba1f4cc8f9e67c670d2344664b
prajwalsoni <prajwalsonics@gmail.com>
2024-01-02 23:37:15 +0530

    Added Auth

--- src/customer/Auth/AuthModal.jsx
+++ src/customer/Auth/AuthModal.jsx
@@ -1,5 +1,7 @@
 import { Box, Modal, Typography } from "@mui/material";
 import React from "react";
+import RegisterForm from "./RegisterForm";
+import { useLocation } from "react-router-dom";
 
 const style = {
     position: 'absolute',
@@ -8,25 +10,24 @@ const style = {
     transform: 'translate(-50%, -50%)',
     width: 500,
     bgcolor: 'background.paper',
-    outline:"none",
+    outline: "none",
     boxShadow: 24,
     p: 4,
-  };
-const AuthModal = ({handleClose,open}) => {
+};
+const AuthModal = ({ handleClose, open }) => {
+    const location = useLocation
     return (
         <div>
             <Modal
-  open={open}
-  onClose={handleClose}
-  aria-labelledby="modal-modal-title"
-  aria-describedby="modal-modal-description"
->
-  <Box sx={style}>
-    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-      Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-    </Typography>
-  </Box>
-</Modal>
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <Box sx={style}>
+                    <RegisterForm />
+                </Box>
+            </Modal>
         </div>
     )
 }
