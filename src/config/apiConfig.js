ff351ab402951e89666e3318d3f1a2eedfa4125b
prajwalsoni <prajwalsonics@gmail.com>
2024-01-18 23:24:46 +0530

    added railway domain

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 export const API_BASE_URL = 'http://localhost:5454'
+//export const API_BASE_URL = 'http://restful-webservices-production.up.railway.app'
 
 const jwt = localStorage.getItem("jwt")
 
