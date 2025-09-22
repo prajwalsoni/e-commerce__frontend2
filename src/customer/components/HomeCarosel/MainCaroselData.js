e436b3a4cb8385a6e522c02567eee2761fb08b25
prajwalsoni <prajwalsonics@gmail.com>
2025-09-23 00:53:34 +0530

    final change in header image

--- src/customer/components/HomeCarosel/MainCaroselData.js
+++ src/customer/components/HomeCarosel/MainCaroselData.js
@@ -1,18 +1,18 @@
 export const mainCarouselData=[
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_5.jpg",
+        image:"https://www.ethnicplus.in/cdn/shop/files/4_4b861929-b1f1-4dc4-9e75-2d45af5e7b28.jpg?v=1758046724&width=2000",
         path:"/women/clothing/lengha_choli"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_4.jpg",
+        image:"https://www.ethnicplus.in/cdn/shop/files/5_1_1fa8829a-909e-4c8c-8091-d8315ec377c7.jpg?v=1758176156&width=2000",
         path:"/women/clothing/women_dress"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/9/_/9_8.jpg",
+        image:"https://www.ethnicplus.in/cdn/shop/files/7_1_0f577066-48af-4ce3-93c8-df26e75ad945.jpg?v=1758176156&width=2000",
         path:"/women/clothing/women_dress"
     },
     {
-        image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/1/11_4.jpg",
+        image:"https://www.ethnicplus.in/cdn/shop/files/6_1_153186f2-c424-4c75-b3c5-9e32b90fdaa9.jpg?v=1758176156&width=2000",
         path:"/women/clothing/women_saree"
     }
 
